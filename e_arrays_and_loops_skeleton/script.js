38472ba3185a7cbe1ad2d4a597233024c28e634a
Leong Martin <leongmartin@Leongs-MacBook-Air.local>
2024-04-12 09:19:09 +0800

    added skeleton for arrays and loops activities

---
+++ e_arrays_and_loops_skeleton/script.js
@@ -0,0 +1,61 @@
+// ---------
+// 1. ARRAYS
+// ---------
+
+// Basic array
+// This is just an example, the best practice is to store a single data type to an array
+const myArray = [1, 2, 3, 4, "Hello", true, null];
+console.log(myArray)
+
+// An array SHOULD be used with values of a single data type
+const myArray2 = ["James", "Sam", "Xavier"];
+
+// A) ARRAY PROPERTIES
+console.log("Number of elements in mappedArray: " + myArray2.length)
+
+
+// B) ARRAY METHODS
+
+// i - map function of an array
+// Implement your code here
+
+
+// ii - filter function of an array
+// Implement your code here
+
+
+// iii - sort function of an array
+// Implement your code here
+
+// Sort by descending order
+// Implement your code here
+
+// Sort by descending order
+// Implement your code here
+
+
+// iv - reduce function of an array
+// Implement your code here
+
+// another approach of applying a reuduce function of an array
+// Implement your code here
+
+
+// ---------
+// 2. LOOPS
+// ---------
+
+// i - for loops (Simulate a countdown using a for loop)
+// Implement your code here
+
+
+// ii - for-each loop
+// Implement your code here
+
+
+// iii - do-while loop (execute first, check for the condition later)
+// Implement your code here
+
+
+// iv - while loop (check the conditions first before running the while loop)
+// Implement your code here
