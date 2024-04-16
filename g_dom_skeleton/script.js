33ddca8c31abc8629fc57cc2604b0cfb3918402b
Leong Martin <leongmartin@Leongs-MacBook-Air.local>
2024-04-16 09:12:38 +0800

    added examples for DOM activities

---
+++ g_dom_skeleton/script.js
@@ -0,0 +1,77 @@
+// DOM Manipulation:
+document.body.append("Appended string.");
+
+const pagetitle = document.getElementById("pageTitle");
+
+// 1. element.innerHTML
+// Setting HTML markup contained within the element.
+
+// Implement Code here
+
+
+// 2. element.innerText
+// Rendering text content of a node and its descendants.
+
+// Implement Code here
+
+
+// 3. element.style
+// Modifying the style of HTML elements
+
+// Implement Code here
+
+
+// 4. getElementById
+// refer to the example above (line 4)
+
+
+// 5. getElementsByTagName
+// Using getElementsByTagName method, search for elements that match a tag name
+
+// Implement Code here
+
+// Using getElementsByTagName method, search for elements that match a tag's index
+
+// Implement Code here
+
+
+// 6. createElement
+// Adding a new HTML element in JavaScript.
+
+// Implement Code here
+
+
+// 7. querySelector
+// Selecting the fist instance of h2 tag and adding an innerText
+
+// Implement Code here
+
+// Using querySelectorAll method, search for elements that match a tag's class
+
+// Implement Code here
+
+
+// 8. element.append()
+// Adding a new HTML element inside an existing element.
+
+// Implement Code here
+
+
+// 9. parentNode.appendChild(node)
+// Adding a node using appendChild
+
+// Implement Code here
+
+
+// ----------------------------------------------
+// Refer to ./form/form.html and ./form/script.js
+// ----------------------------------------------
+
+// 10. element.setAttribute()
+// Setting the Attributes of HTML select input
+
+// 11. element.getAttribute()
+// get the Attributes's value(s) of selected options
+
+// 12. element.addEventListener()
+// Adding an event listener to a button
