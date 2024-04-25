c5ab03c648b6c9fbc2651de6e89a0e3854f62dc7
Leong Martin <leongmartin@Leongs-MacBook-Air.local>
2024-04-25 09:39:49 +0800

    added activity for unit testing in javascript

---
+++ l_unit_test_skeleton/validation.js
@@ -0,0 +1,22 @@
+// 1. Regular expression to check if value is empty or a white space: https://www.freecodecamp.org/news/how-to-include-empty-string-in-regex/
+// The test() method of RegExp instances executes a search with this regular expression for a match between a regular expression and a specified string.
+
+function isEmpty(value){
+    // Fill in the validation condition using regular expressions
+    // And return the results
+};
+
+// 2. Example regular expression: isEmail
+// Reference: https://emaillistvalidation.com/blog/mastering-email-validation-in-bootstrap-5-a-comprehensive-guide/
+
+function isEmail(value){
+    // Fill in the validation condition using regular expressions
+    // AND return the results
+}
+
+// Further reading: 
+// 1. Puppeteer library: https://jestjs.io/docs/puppeteer
+// 2. Regular expression generator: https://saasbase.dev/tools/regex-generator
+// 3. NTU examples: https://www3.ntu.edu.sg/home/ehchua/programming/howto/Regexe.html
+// 4. https://jestjs.io/docs/asynchronous
+// 4.1. Asynchronous functions: https://jestjs.io/docs/tutorial-async
