bea500ee86360db22608e0b04baa4a9e34ba84a7
Leong Martin <leongmartin@Leongs-MacBook-Air.local>
2024-08-26 15:05:30 +0800

    updated answer for controlflow_challenge_answer

--- d_controlflow_challenge_answer/script.js
+++ d_controlflow_challenge_answer/script.js
@@ -7,51 +7,59 @@
   found in index.html
 */
 function ftnSalutation(){
-    return new Array("Mr.", "Mrs.", "Ms.", "Dr.");
+  return new Array("Mr.", "Mrs.", "Ms.", "Dr.");
 }
 
 /* 
-  ftnSalutation written as an ARROW function
-  it uses the passed-in parameters to form
-  a greeting to be returned to the function that
-  calls it
+ftnSalutation written as an ARROW function
+it uses the passed-in parameters to form
+a greeting to be returned to the function that
+calls it
 */
 const ftnGreeting = (salutation, user) => {
-    var msg = "Hi, " + salutation + " " + user + "! We received your submssion.";
-    return msg; 
+  const msg = "Hi, " + salutation + " " + user + "! We received your submssion.";
+  return msg; 
 }
 
 // Refactor the if conditions, use comparison operators to form a single if statement
 function validateInputs(choice, name = ""){
 
-  //  if the choice is empty OR the name is empty, return to the caller prematurely
-  if(choice == "" || name=="")
-    return;
+//  if the choice is empty OR the name is empty, return to the caller prematurely
+if(choice == "" || name=="")
+  return;
 
-  return true;
+return true;
 
 }
 
-// Refactor the function to use a switch statement
+// TODO: Use a function where a switch statement is used for the following
 // None:    "Thank you. No preference selected."
 // Chicken: "The main course served is Chicken Cordon Bleu."
 // Beef:    "The main course served is Beef Wellington." 
 // Others:  "We will contact you separately to arrange."
 function ftnRadioMeal(meal){
 
-  // using switch statement to provide the appropriate feedback to the user
-  switch (meal) { // key is the meal-passed in
-    case "None":
-      document.getElementById("displayPreference").textContent = "Thank you. No preference selected.";
-      break;
-    case "Beef":
-      document.getElementById("displayPreference").textContent = "The main course served is Beef Wellington."; 
-      break;
-      case "Chicken":
-        document.getElementById("displayPreference").textContent = "The main course served is Chicken Cordon Bleu.";
-        break;
-    default:
-      document.getElementById("displayPreference").textContent = "We will contact you separately to arrange."
-      break;
-  }
+let msg = "Thank you. ";
+
+// TODO: using switch statement to provide the appropriate feedback to the user
+switch (meal) {
+  case "Vegetarian":
+    // Code block for Vegetarian
+    msg = msg + "Please contact us to arrange.";
+    break;
+  case "Beef":
+    // Code block for Beef
+    msg = msg + "We'll be serving Beef Wellington.";
+    break;
+  case "Chicken":
+    // Code block for Chicken
+    msg = msg + "We'll be serving Chicken Corden Blue.";
+    break;
+  default:
+    // Code block for None
+    msg = msg + "No preference selected.";
+    break;
+}
+
+return document.getElementById("displayPreference").textContent = msg;
 }
