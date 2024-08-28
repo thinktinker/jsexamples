241ba6b230c858bcc9a11fd2f46c3fa4b77b15d6
Martin Leong <martinleongmo@gmail.com>
2024-08-28 12:53:53 +0800

    Update index.js
    comments added for the activity completed.
    use-case added for the use of OS and ENV constants.

--- i-node_package_manager/index.js
+++ i-node_package_manager/index.js
@@ -1,33 +1,47 @@
-// before running the app
-//npm install express
+'use strict';   // enable strict mode is to ensure the code written is more secure and less prone to errors and "unsafe" actions.
 
-// to run 
-// npm start
-
-'use strict';
-
-const express = require('express');
+const express = require("express");
 
 // Constants
-const PORT = 8080;
-const HOST = '127.0.0.1';
-const OS = require('os');
-const ENV = 'DEV';
+const PORT = 8989;
+const HOST = "127.0.0.1";
+const OS = require("os");                                           // console.log(OS.platform()); // e.g., 'darwin'
+const ENV = "DEV";                                                  // determines the mode of development - e.g. below
 
+// Application where we have enpoints that users can look for
+const app = express();  // invoke the express library through instantiating it as a variable called app
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello FSD Learner! Running Node.js!';
-  res.send(msg);
+app.get("/", (req, res)=>{                                           // listens for http://localhost:8989/ to be called
+    res.statusCode = 200;                                            // return a response.ok
+    const msg = "Hello FSD Learner! Running Node.js";                // create a message to be returned
+    res.send(msg);                                                   // send the message to the user
 });
 
-app.get('/test', (req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello from /test Node!';
-  res.send(msg);
-});
+app.get("/test", (req, res)=>{                                      // listens for http://localhost:8989/test to be called
+    res.statusCode = 200                                            // returns a response.ok                                            
+    const msg = "Hello from /test Node.js!";                        // create a message to be returned
+    res.send(msg);                                                  // send the message to the user
+});              
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+// console.log("Running on PORT:" + PORT + " and HOST:" + HOST);    // conventional way to concatenate strings
+console.log(`Runs on http://${HOST}:${PORT}`);                      // using template strings to concatenate strings
+console.log(OS.platform());
+
+/* 
+Example on the use of a constant to manage the deployment environment that an app is run in.
+In the example below, ENV variable is used to determine the exact database to use whilst in development.
+Note: Other possible values include: "PROD", "UAT"
+
+const ENV = "DEV";
+
+const config = {
+  databaseConfig: {
+    host: ENV === "DEV" ? "localhost" : "prod-db-host",
+    user: ENV === "DEV" ? "dev_user" : "prod_user",
+    password: ENV === "DEV" ? "dev_password" : "prod_password",
+    database: ENV === "DEV" ? "dev_database" : "prod_database"
+  },
+  logLevel: ENV === "DEV" ? "debug" : "error"
+};
+*/
